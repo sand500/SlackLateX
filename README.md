84d799450cff8622e95e202c63a06afb80ddc803
Harsh Poddar <me@harsh.io>
2017-02-11 17:16:52 -0500

    Added features, modified behavior and cleaned up code.

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
-# SlackLateX
-Bot to convert LateX code in messages into 
+# Slack LaTeX
+#### Bot to convert LaTeX code in messages into:
 
 ![screen]
 
-##How to use:
-Install required modules with:
-```
-npm install fs
-npm install request
-npm install websocket
-```
+### Setup:
+1. Clone/download this repository.
+2. Install required modules in the project folder with:
+    ```
+    npm install fs
+    npm install request
+    npm install websocket
+    ```
+3. Create a `secret.txt` file in the bot directory and put your slack token in it.
+4. Start bot by running `node bot.js` or `nodejs bot.js`
 
-Create a secret.txt file in the bot directory and put your slack token in it
+### Usage:
 
-Start bot by running `node bot.js` or `nodejs bot.js`
+Add the bot to the channel you want to enable SlackLaTeX in. Any message with the following format will be turned into an image when bot is enabled.
+ * `$\LaTeX code$`
+ * `` `$\LaTeX code$` ``
+ * `` ```$\LaTeX code$``` ``
 
-In Slack, type `..startLatex` to enable bot and `..stopLatex` to disable bot. This only applies the user and channel that typed this. 
+You can also edit or delete a message and the bot will do the right thing.
 
-Any message that starts with '$' and ends with '$' will be turned into an image when bot is enabled
-
+In Slack, type `..stopLatex` to disable bot and `..startLatex` to re-enable bot. This only applies the user and channel that typed this. By default, it will work for all users in every channel the box exists.
 
 [screen]:http://i.imgur.com/7xbkJ6P.png
--- bot.js
+++ bot.js
@@ -1,141 +1,224 @@
-var request = require("request");
+let request = require("request");
 
-//var WebSocket = require('ws');
-var WebSocketClient = require('websocket').client;
-var ws;
-var message="";
-var i = 0;
-var latex = {};
-var fs = require('fs');
-fs.readFile('secret.txt','utf8',function (err, data) {
-    global.token=data;
-    global.token = global.token.replace(/[\n\r]/g,"");
+//let WebSocket = require('ws');
+let WebSocketClient = require('websocket').client;
+let ws;
+let message = "";
+let i = 0;
+let latex = {};
+let fs = require('fs');
+fs.readFile('secret.txt', 'utf8', function(err, data) {
+    global.token = data;
+    global.token = global.token.replace(/[\n\r]/g, "");
     getWebSocket();
 });
 
-var connection = null;
+let connection = null;
 
-//gets the websocket url 
-function getWebSocket(){
-    request('https://slack.com/api/rtm.start?token='+global.token+'&pretty=1', function (error, response, body) {
-         //console.log(response.url);
-         if (!error && response.statusCode == 200) {
+let replies = {}
+
+//gets the websocket url
+function getWebSocket() {
+    request('https://slack.com/api/rtm.start?token=' + global.token + '&pretty=1', function(error, response, body) {
+        log(response.url);
+        if (!error && response.statusCode === 200) {
             url = JSON.parse(body).url;
-            console.log( "Creating url:"+url);
+            log( "Creating url:"+url);
             createWS(url);
-         }
+        }
     });
 }
 
 
 //initiates websocket connection
 function createWS(url) {
-    var client = new WebSocketClient();
+    let client = new WebSocketClient();
 
     client.on('connectFailed', function(error) {
-        console.log('Connect Error: ' + error.toString());
+        log('Connect Error: ' + error.toString());
     });
 
     client.on('connect', function(conn) {
-	connection = conn;
-        console.log('WebSocket Client Connected');
+        connection = conn;
+        log('WebSocket Client Connected');
 
         connection.on('error', function(error) {
-            console.log("Connection Error: " + error.toString());
+            log("Connection Error: " + error.toString());
         });
 
         connection.on('close', function() {
-            console.log('echo-protocol Connection Closed');
+            log('echo-protocol Connection Closed');
         });
 
         connection.on('message', function(message) {
             if (message.type === 'utf8') {
-                handleMessage(JSON.parse(message.utf8Data),message);
+                handleMessage(JSON.parse(message.utf8Data), message);
             }
 
         });
     });
     client.connect(url);
-    console.log("hey?");
+    log("hey?");
 
 }
 
-function deleteMessage(timestamp,channel) {
-    var dURL = "https://slack.com/api/chat.delete?token="+global.token+"&ts=" +timestamp+"&channel="+channel+"&pretty=1";
-
-    request(dURL, function (error, response, body) {
-         //console.log(response.url);
-         if (!error && response.statusCode == 200) {
-            console.log(body);
-         }
-        });
+function deleteMessage(timestamp, channel) {
+    let dURL = "https://slack.com/api/chat.delete?token=" + global.token + "&ts=" + timestamp + "&channel=" + channel + "&pretty=1";
+    log("Message to be deleted: "+timestamp + "\n");
+    request(dURL, function(error, response, body) {
+        log(response.url);
+        if (!error && response.statusCode === 200) {
+            log(body);
+        }
+    });
 }
 
-function postLatex(channel,text) {
-    var urlBase= 'http://latex.codecogs.com/png.latex?%5Cdpi%7B300%7D%20'+encodeURIComponent(text);
+function postLatex(user, channel, text) {
+    let urlBase = 'http://latex.codecogs.com/png.latex?%5Cdpi%7B300%7D%20' + encodeURIComponent(text);
 
-    var dURL = "https://slack.com/api/chat.postMessage?token="+global.token+"&channel="+channel+"&text=%20&attachments=%5B%7B%22fallback%22%3A%22.%22%2C%22color%22%3A%20%22%2336a64f%22%2C%22image_url%22%3A%22" + encodeURIComponent(urlBase)+"%22%7D%5D&pretty=1";
-    
-    request(dURL, function (error, response, body) {
-         //console.log(response.url);
-         if (!error && response.statusCode == 200) {
-            console.log(body);
-         }
-        });
+    let dURL = "https://slack.com/api/chat.postMessage?as_user=true&token=" + global.token + "&channel=" + channel + "&text=%20&attachments=%5B%7B%22fallback%22%3A%22.%22%2C%22color%22%3A%20%22%2336a64f%22%2C%22image_url%22%3A%22" + encodeURIComponent(urlBase) + "%22%7D%5D&pretty=1";
+
+    request(dURL, function(error, response, body) {
+        log(response.url);
+        if (!error && response.statusCode === 200) {
+            log(JSON.parse(body).ts);
+            replies[user + channel].reply_ts = JSON.parse(body).ts;
+            log(body);
+        }
+    });
+}
+
+function updateLatex(channel, text, ts) {
+    let urlBase = 'http://latex.codecogs.com/png.latex?%5Cdpi%7B300%7D%20' + encodeURIComponent(text);
+
+    let dURL = "https://slack.com/api/chat.update?as_user=true&ts=" + ts + "&token=" + global.token + "&channel=" + channel + "&text=%20&attachments=%5B%7B%22fallback%22%3A%22.%22%2C%22color%22%3A%20%22%2336a64f%22%2C%22image_url%22%3A%22" + encodeURIComponent(urlBase) + "%22%7D%5D&pretty=1";
+
+    request(dURL, function(error, response, body) {
+        log(response.url);
+        if (!error && response.statusCode === 200) {
+            log(body);
+        }
+    });
 }
 
 
-function handleMessage(mObj,message){
+function handleMessage(mObj, message) {
 
-    console.log("Received: '" + message.utf8Data + "'");
-    console.log(mObj.type+"\n");
+    log("Received: '" + message.utf8Data + "'");
+    log(mObj.type+"\n");
 
-    if(mObj.type==='message'){
-        
-        console.log("\t"+mObj.channel+"\n");
-        console.log("\t"+mObj.user+"\n");
-        console.log("\t"+mObj.text+"\n");
-        console.log("\t"+escape(mObj.text)+"\n");
+    let channel = mObj.channel;
 
-        if(mObj.text==='..ping'){
-            pong(mObj.channel,"pong");
+    if (mObj.type === 'message' && mObj.subtype === undefined) {
+
+        log("\t"+mObj.channel+"\n");
+        log("\t"+mObj.user+"\n");
+        log("\t"+mObj.text+"\n");
+        log("\t"+escape(mObj.text)+"\n");
+
+        let laText = getLatex(mObj.text);
+
+        if (mObj.text === '..ping') {
+            pong(mObj.channel, "pong");
         }
-        if(latex[mObj.user+mObj.channel]==true && mObj.text[0]==='$' && mObj.text[mObj.text.length-1]==='$' && mObj.text.length>1) {
-            deleteMessage(mObj.ts,mObj.channel);
-			postLatex(mObj.channel,replaceAll(mObj.text.substring(1,mObj.text.length-1),'&amp;','&'));
+        if (latex[mObj.user + mObj.channel] != false && laText != null) {
+            replies[mObj.user + mObj.channel] = {
+                orignal_ts: mObj.ts
+            };
+            postLatex(mObj.user, mObj.channel, replaceAll(laText, '&amp;', '&'));
 
-            console.log('Converting to latex: ' + mObj.text);
-        }
-        
-        if(mObj.text==='..startLatex') {
-            latex[mObj.user+mObj.channel]=true;
-            console.log('Enable latex for ' + mObj.user+mObj.channel);
-        }
-
-        if(mObj.text==='..stopLatex') {
-            latex[mObj.user+mObj.channel]=false;
-            console.log('disable latex for ' + mObj.user+mObj.channel);
+            log('Converting to latex: ' + mObj.text);
         }
 
+        if (mObj.text === '..startLatex') {
+            latex[mObj.user + mObj.channel] = true;
+            log('Enable latex for ' + mObj.user+mObj.channel);
+        }
+
+        if (mObj.text === '..stopLatex') {
+            latex[mObj.user + mObj.channel] = false;
+            log('disable latex for ' + mObj.user+mObj.channel);
+        }
+
+    } else if (mObj.subtype === "message_changed") {
+        let laText = getLatex(mObj.message.text);
+        if (replies[mObj.message.user + mObj.channel] !== undefined && replies[mObj.message.user + mObj.channel].orignal_ts === mObj.message.ts) {
+
+            if (laText != null) { // is latex
+                updateLatex(mObj.channel, replaceAll(laText, '&amp;', '&'),
+                    replies[mObj.message.user + mObj.channel].reply_ts);
+            } else { // not latex
+                deleteMessage(replies[mObj.message.user + mObj.channel].reply_ts, mObj.channel);
+                delete replies[mObj.message.user + mObj.channel];
+            }
+        } else {
+            if (latex[mObj.message.user + mObj.channel] != false && laText != null) {
+                // deleteMessage(mObj.ts,mObj.channel);
+
+                replies[mObj.message.user + mObj.channel] = {
+                    orignal_ts: mObj.message.ts
+                };
+                postLatex(mObj.message.user, mObj.channel, replaceAll(laText, '&amp;', '&'));
+
+                log('Converting to latex: ' + mObj.message.text);
+            }
+        }
+    } else if (mObj.subtype === "message_deleted") {
+        if (replies[mObj.previous_message.user + mObj.channel] !== undefined && replies[mObj.previous_message.user + mObj.channel].orignal_ts === mObj.previous_message.ts) {
+            deleteMessage(replies[mObj.previous_message.user + mObj.channel].reply_ts, mObj.channel);
+            delete replies[mObj.previous_message.user + mObj.channel];
+        }
     }
 
 }
-function pong(channel,text){
+
+const latexWrapRegex = /^\$(.*)\$$|^`\s*\$(.*)\$\s*`$|^```\s*\$(.*)\$\s*```$/g;
+
+function getLatex(text) {
+    let m;
+
+    let laText = null;
+
+    while ((m = latexWrapRegex.exec(text)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === latexWrapRegex.lastIndex) {
+            latexWrapRegex.lastIndex++;
+        }
+
+        // The result can be accessed through the `m`-variable.
+        m.forEach((match, groupIndex) => {
+            if (groupIndex !== 0 && match !== undefined) {
+                laText = match;
+                return;
+            }
+        });
+    }
+    return laText;
+}
+
+function pong(channel, text) {
     if (connection.connected) {
-        var message2send={};
-        message2send.text=text;
-        message2send.channel=channel;
-        message2send.id=i;
-        message2send.type="message";
-        connection.sendUTF( JSON.stringify(message2send));
-        i+=1;
+        let message2send = {};
+        message2send.text = text;
+        message2send.channel = channel;
+        message2send.id = i;
+        message2send.type = "message";
+        connection.sendUTF(JSON.stringify(message2send));
+        i += 1;
         return i;
     }
 }
 
+const escapeRegEx = /([.*+?^=!:${}()|[]\/\])/g;
+
 function escapeRegExp(str) {
-	return str.replace(/([.*+?^=!:${}()|[]\/\])/g, "\$1");
+    return str.replace(escapeRegEx, "\$1");
 }
+
 function replaceAll(str, find, replace) {
-	return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function log (obj) {
+    // console.log(obj);
 }
